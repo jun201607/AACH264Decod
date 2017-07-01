3369e670a9f7ebfb2377e9ba06b90db7a108af61
lijun <913811215@qq.com>
2017-07-01 12:02:34 +0800

    提交

---
+++ Test/app/src/main/java/com/sona/test/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sona.test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.sona.readFileDecode.AACFileDecoderActivity;
+import com.sona.readFileDecode.H264FileDecodeActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play_video:
+                Intent i = new Intent(this, H264FileDecodeActivity.class);
+                startActivity(i);
+                break;
+            case R.id.play_audio:
+                Intent i1 = new Intent(this, AACFileDecoderActivity.class);
+                startActivity(i1);
+                break;
+        }
+    }
+}
